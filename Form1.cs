f1cb5cc73d334874dc8c645d3b04916f80adf534
Siemen K <siemenkraayenbrink@gmail.com>
2015-11-08 19:27:43 +0100

    massive stufffff

--- Form1.cs
+++ Form1.cs
@@ -16,11 +16,13 @@ namespace INFOIBV
         private Bitmap InputImage;
         private Bitmap OutputImage;
         Color[,] Labels;
-
+        // Dictionary that holds the pixel values that belong to the object with label as key
+        Dictionary<int, List<Tuple<int, int>>> Objects;
         public INFOIBV()
         {
             InitializeComponent();
-            
+            // Initialize objects dictionary
+            Objects = new Dictionary<int, List<Tuple<int, int>>>();
         }
 
         private void LoadImageButton_Click(object sender, EventArgs e)
@@ -72,7 +74,7 @@ namespace INFOIBV
                     Color pixelColor = Image[x, y];                         // Get the pixel color at coordinate (x,y)
                     Color updatedColor;
                     //Thresholding, we assume we have bright triangles
-                    if (pixelColor.R < 180)
+                    if (pixelColor.R < 90)
                     {
                         updatedColor = Color.FromArgb(0, 0, 0);
                     }
@@ -92,7 +94,7 @@ namespace INFOIBV
             Color[,] CopyImage = new Color[InputImage.Size.Width, InputImage.Size.Height];
             Array.Copy(Image, 0, CopyImage, 0, Image.Length);
             //++++++EROSION++++++
-            for (int x = 0; x < InputImage.Size.Width-1; x++)
+            /*for (int x = 0; x < InputImage.Size.Width-1; x++)
             {
                 for (int y = 0; y < InputImage.Size.Height-1; y++)
                 {
@@ -121,10 +123,76 @@ namespace INFOIBV
                     }
                 }
             }
+            */
+            for (int x = 0; x < InputImage.Size.Width - 1; x++)
+            {
+                for (int y = 0; y < InputImage.Size.Height - 1; y++)
+                {
+                    Color pixelColor = Image[x, y];
+                    if (pixelColor == white)
+                    {
+                        // Found a white pixel.
+                        // Check if we are in bounds foy y
+                        if (y > 1 && y < InputImage.Size.Height - 2)
+                        {
+                            // Check neighbours (we're sort of evaluating 3 values at a time to decide the color of the middle one)
+                            if (Image[x, y - 2] == white && Image[x, y - 1] == white && Image[x, y + 1] == white && Image[x, y + 2] == white)
+                                CopyImage[x, y] = white;
+                            else
+                                CopyImage[x, y] = black;
+
+                        }
+                        // Same stuff for x
+                        if (x > 1 && x < InputImage.Size.Width - 2)
+                        {
+                            if (Image[x - 2, y] == white && Image[x - 1, y] == white && Image[x + 1, y] == white && Image[x + 2, y] == white)
+                                CopyImage[x, y] = white;
+                            else
+                                CopyImage[x, y] = black;
+                        }
+                    }
+                }
+            }
             // Flush the erosion to the image
             Array.Copy(CopyImage, 0, Image, 0, CopyImage.Length);
             //++++++END OF ERIOSION+++++
             //++++++DILATION++++++
+            /*for (int x = 0; x < InputImage.Size.Width - 1; x++)
+            {
+                for (int y = 0; y < InputImage.Size.Height - 1; y++)
+                {
+                    Color pixelColor = Image[x, y];
+                    if (pixelColor == white)
+                    {
+                        // Check bounds for y
+                        if (y > 1 && y < InputImage.Size.Height-2)
+                        {
+                            // Do the actual dilation for y
+                            if (Image[x, y - 2] == black)
+                                CopyImage[x, y - 2] = white;
+                            if (Image[x, y - 1] == black)
+                                CopyImage[x, y - 1] = white;
+                            if (Image[x, y + 1] == black)
+                                CopyImage[x, y + 1] = white;
+                            if (Image[x, y + 2] == black)
+                                CopyImage[x, y + 2] = white;
+                        }
+                        if (x > 1 && x < InputImage.Size.Width-2)
+                        {
+                            // Do the actual dilation for x
+                            if (Image[x - 2, y] == black)
+                                CopyImage[x - 2, y] = white;
+                            if (Image[x - 1, y] == black)
+                                CopyImage[x - 1, y] = white;
+                            if (Image[x + 1, y] == black)
+                                CopyImage[x + 1, y] = white;
+                            if (Image[x + 2, y] == black)
+                                CopyImage[x + 2, y] = white;
+                        }
+                    }
+                }
+            }
+            */
             for (int x = 0; x < InputImage.Size.Width - 1; x++)
             {
                 for (int y = 0; y < InputImage.Size.Height - 1; y++)
@@ -155,10 +223,96 @@ namespace INFOIBV
             // Flush the dilation to the image
             Array.Copy(CopyImage, 0, Image, 0, CopyImage.Length);
             //++++++END OF DILATION++++++
+            //watershed
+            int[,] watershedImg = new int[InputImage.Size.Width, InputImage.Size.Height];
+
+            for (int x = 0; x < InputImage.Size.Width - 1; x++)
+            {
+                for (int y = 0; y < InputImage.Size.Height - 1; y++)
+                {
+                    Color pxcolor = Image[x, y];
+                    if (pxcolor == white)
+                        watershedImg[x, y] = 512;
+                    else
+                        watershedImg[x, y] = 0;
+                }
+            }
+
+
+            for (int x = 0; x < InputImage.Size.Width - 1; x++)
+            {
+                for (int y = 0; y < InputImage.Size.Height - 1; y++)
+                {
+                    Color pxcolor = Image[x, y];
+
+                    //padding
+                    if (x == 0 && y == 0)
+                        watershedImg[x, y] = watershedImg[x + 1, y + 1];
+                    else if (x == 0)
+                        watershedImg[x, y] = watershedImg[x + 1, y];
+                    else if (y == 0)
+                        watershedImg[x, y] = watershedImg[x, y + 1];
+                    else
+                    {
+                        watershedImg[x, y] = Math.Min(Math.Min(Math.Min(Math.Min(
+                                                watershedImg[x - 1, y - 1] + 2,
+                                                watershedImg[x, y - 1] + 1),
+                                                watershedImg[x + 1, y - 1] + 2),
+                                                watershedImg[x - 1, y] + 1),
+                                                watershedImg[x, y]);
+                    }
+                }
+            }
+
+
+            for (int x = 0; x < InputImage.Size.Width - 1; x++)
+            {
+                for (int y = 0; y < InputImage.Size.Height - 1; y++)
+                {
+                    Color pxcolor = Image[x, y];
+                    if (x == 0 && y == 0)
+                        watershedImg[x, y] = watershedImg[x + 1, y + 1];
+                    else if (x == 0)
+                        watershedImg[x, y] = watershedImg[x + 1, y];
+                    else if (y == 0)
+                        watershedImg[x, y] = watershedImg[x, y + 1];
+                    else
+                    {
+                        watershedImg[x, y] = Math.Min(Math.Min(Math.Min(Math.Min(
+                                            watershedImg[x, y],
+                                            watershedImg[x + 1, y] + 1),
+                                            watershedImg[x - 1, y + 1] + 2),
+                                            watershedImg[x, y + 1] + 1),
+                                            watershedImg[x + 1, y + 1] + 2);
+                    }
+                }
+            }
+            // Complement distance transform with binary
+            for (int x = 0; x < InputImage.Size.Width-1; x++)
+            {
+                for (int y = 0; y < InputImage.Size.Height-1; y++)
+                {
+                    watershedImg[x, y] = 255 - watershedImg[x, y];
+                }
+            }
+
+            // Seperate where distances meet
+            /*for (int x = 3; x < InputImage.Size.Width-3; x++)
+            {
+                for (int y = 3; y < InputImage.Size.Width-3; y++)
+                {
+                    if (watershedImg[x + 1, y] == watershedImg[x - 2, y] && watershedImg[x, y] == watershedImg[x - 1, y] && watershedImg[x - 3, y] == watershedImg[x + 2, y])
+                    {
+                        watershedImg[x, y] = 0;
+                        watershedImg[x - 1, y] = 0;
+                    }
+ 
+                }
+            }*/
             //++++++LABEL++++++
             Labels = new Color[InputImage.Size.Width, InputImage.Size.Height];
             Array.Copy(Image, 0, Labels, 0, Image.Length);
-            Color labelColor = Color.FromArgb(3,3,3);
+            Color labelColor = Color.FromArgb(1,1,1);
             int width = InputImage.Size.Width;
             int height = InputImage.Size.Height;
             for (int x = 0; x < InputImage.Size.Width - 1; x++)
@@ -170,7 +324,7 @@ namespace INFOIBV
                     {
 
                         var thread = new Thread(
-                        () => Label(labelColor, x, y, width, height), 10000000);
+                        () => Label(labelColor, x, y, width, height), 100000000);
                         thread.Start();
                         thread.Join();
                         Label(labelColor, x, y, width, height);
@@ -188,15 +342,39 @@ namespace INFOIBV
                             return;
                         }
                         //thread.Abort();
-                        labelColor = Color.FromArgb(labelColor.R + 5, labelColor.G + 5, labelColor.B + 5);
+                        labelColor = Color.FromArgb(labelColor.R + 1, labelColor.G + 1, labelColor.B + 1);
                         
                     }
                 }
             }
+            Console.WriteLine("Objects labeled: " + Objects.Count);
             // Flush labels to image
             Array.Copy(Labels, 0, Image, 0, Labels.Length);
-            //TODO: step 1: label remaining objects
-            //TODO: step 2: analyze labeled objects. remove not "triangily" objects
+            // Check if the objects are triangles.
+            List<List<Tuple<int,int>>> triangles = new List<List<Tuple<int,int>>>();
+            foreach (KeyValuePair<int, List<Tuple<int,int>>> pair in Objects)
+            {
+                if (IsTriangle(pair.Value))
+                    triangles.Add(pair.Value);
+            }
+            Console.WriteLine("Triangles: " + triangles.Count);
+            // Set output image to black
+            for (int x = 0; x < InputImage.Size.Width; x++)
+            {
+                for (int y = 0; y < InputImage.Size.Height; y++)
+                {
+                    OutputImage.SetPixel(x, y, Color.FromArgb(0,0,0));               // Set the pixel color at coordinate (x,y)
+                }
+            }
+            // Paint triangles in output
+            foreach (List<Tuple<int, int>> triangle in triangles)
+            {
+                foreach (Tuple<int, int> pixel in triangle)
+                {
+                    OutputImage.SetPixel(pixel.Item1,pixel.Item2,Color.FromArgb(255,255,255));
+                }
+            }
+            //TODO: step 1: analyze labeled objects. remove not "triangily" objects
             
 
             //==========================================================================================
@@ -206,11 +384,49 @@ namespace INFOIBV
             {
                 for (int y = 0; y < InputImage.Size.Height; y++)
                 {
-                    OutputImage.SetPixel(x, y, Image[x, y]);               // Set the pixel color at coordinate (x,y)
+                    //OutputImage.SetPixel(x, y, Image[x, y]);
+                    OutputImage.SetPixel(x, y, Color.FromArgb(watershedImg[x, y],watershedImg[x,y],watershedImg[x,y]));// Set the pixel color at coordinate (x,y)
                 }
             }
             pictureBox2.Image = (Image)OutputImage;                         // Display output image
             progressBar.Visible = false;                                    // Hide progress bar
+        }
+
+        private bool IsTriangle(List<Tuple<int, int>> o)
+        {
+            int minX = Int32.MaxValue;
+            int minY = Int32.MaxValue;
+            int maxX = 0;
+            int maxY = 0;
+            float area = 0;
+            foreach (Tuple<int, int> coord in o)
+            {
+                area++;
+                if (coord.Item1 < minX)
+                    minX = coord.Item1;
+                if (coord.Item1 > maxX)
+                    maxX = coord.Item1;
+                if (coord.Item2 < minY)
+                    minY = coord.Item2;
+                if (coord.Item2 > maxY)
+                    maxY = coord.Item2;
+            }
+            // Calculate the area of the bounding box.
+            int sideX = maxX - minX;
+            int sideY = maxY - minY;
+            int areaRect = sideX * sideY;
+            if (sideX == 0 || sideY == 0) {
+                // Object is a line, therefore no triangle
+                return false;
+            }
+            float devider = area / areaRect;
+            Console.WriteLine("sideX: "+ sideX + " sideY: "+ sideY + " area: "+ area + " areaRect: "+ areaRect +" devider: " + devider);
+            if (devider > .45f && devider < .55f)
+            {
+                // Probably a triangle!
+                return true;
+            }
+            return false;
         }
 
         private void Label(Color color, int x, int y, int width, int height)
@@ -221,7 +437,20 @@ namespace INFOIBV
                 return;
             if (x < width - 1 && y < height -1 && Labels[x, y] == Color.FromArgb(255, 255, 255))
             {
+                // Set the label
                 Labels[x, y] = color;
+                // Set the dictionary entry
+                List<Tuple<int,int>> list;
+                Objects.TryGetValue(color.R, out list);
+                if (list != null)
+                {
+                    list.Add(new Tuple<int, int>(x, y));
+                }
+                else
+                {
+                    Objects.Add(color.R, new List<Tuple<int, int>>());
+                    Objects[color.R].Add(new Tuple<int, int>(x, y));
+                }
                 if (x - 1 > 0 && Labels[x - 1, y] == Color.FromArgb(255,255,255))
                     Label(color, x - 1, y, width, height); // Left neighbour
                 if (Labels[x + 1, y] == Color.FromArgb(255, 255, 255))
